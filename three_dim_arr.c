52d44572f228f7ca57b7df09e69a1daeb361c8a0
Pritam <pritammahata12@gmail.com>
2023-07-23 21:40:30 +0530

    remove line

--- three_dim_arr.c
+++ three_dim_arr.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    int x[2][3][2]={
-        {{0,1},{2,3},{4,5}},
-        {{6,7},{8,9},{10,11}}
-    };
+    int x[2][3][2] = {
+        {{0, 1}, {2, 3}, {4, 5}},
+        {{6, 7}, {8, 9}, {10, 11}}};
 
-    //display of 3dimention array
+    // display of 3dimention array
 
     for (int i = 0; i < 2; i++)
     {
@@ -14,9 +13,9 @@ int main()
         {
             for (int k = 0; k < 2; k++)
             {
-                printf("The Element of x[%d][%d][%d] = %d",i,j,k,x[i][j][k]);
+                printf("The Element of x[%d][%d][%d] = %d", i, j, k, x[i][j][k]);
                 printf("\n");
-            }   
+            }
         }
     }
     return 0;
